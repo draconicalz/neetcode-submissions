ce53f6d15c4a23bc25883429585ab8e63f35a9aa
Michael Odya <38409731+draconicalz@users.noreply.github.com>
2026-06-30 20:46:08 -0500

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+
+        def cap(r, c):
+            if (r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != "O"): return
+
+            board[r][c] = "T"
+            cap(r + 1, c)
+            cap(r - 1, c)
+            cap(r , c + 1)
+            cap(r, c - 1)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O" and (r in [0, ROWS - 1] or c in [0, COLS - 1]):
+                    cap(r, c)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O": board[r][c] = "X"
+                elif board[r][c] == "T": board[r][c] = "O"
+
