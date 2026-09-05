cc8fdca60da6d74679ceedd102856e87e1a6ac28
Michael Odya <38409731+draconicalz@users.noreply.github.com>
2026-09-05 18:38:26 -0500

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.py
@@ -0,0 +1,5 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        split = s.split(" ")
+        for i in range(len(split) - 1, -1, -1):
+            if split[i] != "": return len(split[i])
