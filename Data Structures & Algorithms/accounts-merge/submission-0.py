fa242dc97a9ac1685182ae131f927b1b292efff3
Michael Odya <38409731+draconicalz@users.noreply.github.com>
2026-09-09 17:12:22 -0500

    Add: accounts-merge - submission-0

---
+++ Data Structures & Algorithms/accounts-merge/submission-0.py
@@ -0,0 +1,48 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = [n for n in range(n)]
+        self.rank = [1] * n
+    
+    def find(self, x):
+        while x != self.par[x]:
+            self.par[x] = self.par[self.par[x]]
+            x = self.par[x]
+        return x
+    
+    def union(self, x1, x2):
+        p1, p2 = self.find(x1), self.find(x2)
+
+        if p1 == p2:
+            return False
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        return True
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UnionFind(len(accounts))
+        emails = {}
+
+        for i, a in enumerate(accounts):
+            for e in a[1:]:
+                if e in emails:
+                    uf.union(i, emails[e])
+                else:
+                    emails[e] = i
+        
+        print(emails)
+        emailGroup = defaultdict(list)
+        for e, i in emails.items():
+            leader = uf.find(i)
+            emailGroup[leader].append(e)
+        print(emailGroup)
+        res = []
+        for i, emails in emailGroup.items():
+            name = accounts[i][0]
+            res.append([name] + sorted(emailGroup[i]))
+        return res
+        
